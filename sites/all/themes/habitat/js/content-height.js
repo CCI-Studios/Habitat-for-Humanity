3e0ebdaab6ac6bec18c8535bc2d12db39158ae5c
Chris Morris <cmorris@ccistudios.com>
2014-08-14 14:10:54 -0400

    Match the height of content and sidebar
    Only in full desktop size
    
    close #63

---
+++ sites/all/themes/habitat/js/content-height.js
@@ -0,0 +1,23 @@
+(function($) {
+    $(function(){
+        fixContentHeight();
+    });
+    $(window).load(fixContentHeight);
+
+    function fixContentHeight()
+    {
+        var $content = $(".content_main");
+        var $sidebar = $(".sidebar");
+        var content_height = $content.height();
+        var sidebar_height = $sidebar.height();
+
+        if (content_height < sidebar_height)
+        {
+            $content.height(sidebar_height);
+        }
+        else
+        {
+            $sidebar.height(content_height);
+        }
+    }
+}(jQuery));
